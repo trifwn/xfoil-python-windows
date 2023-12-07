9bc2dfe3f03d057f8879932cbcc7f24dceda238a
trifwn <tryfonthem@gmail.com>
2023-12-08 00:48:23 +0200

    Include the library.

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ def install(package: str, version: str) -> None:
         name=package,
         version=version,
         packages=['xfoil'],
+        include_package_data=True,
     )
 
 def uninstall(package: str) -> None:
